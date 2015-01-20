47dae648633946de2324a0774340941247bb49f1
root <root@localhost.localdomain>
2015-01-21 00:06:35 +0800

    update webscratch for mysql

--- python/webscratch/webscrabe.py
+++ python/webscratch/webscrabe.py
@@ -1,22 +1,71 @@
 import urllib2
 import re
 import mysql.connector
+import time
 
-def check_exist(url):
-	conn=mysql.connector.connect(usr='root',password='',databases='webdb',use_unicode=True)
+
+unusedurl=[]
+g_mysql="usr='root',password='',databases='webdb',use_unicode=True"
+
+def check_exist(urls):
+	conn=mysql.connector.connect(g_mysql)
 	tempcur=conn.cursor()
-	urls=url.split(';')
 	for temp in urls:
-		tempcur.execute('select * from usedurl where usedurl =')
+		tempcur.execute('select * from usedurl where usedurl ='+temp)
 		result=tempcur.fetchall()
-			
-		
-conn=mysql.connector.connect(user='root',password='',databases='webdb',use_unicode=True)
-cursor=conn.cursor()
-cursor.execute('select * from unusedurl limit 100')
-unusedurl=cursor.fetchall()
+		if result:
+			continue
+		else:
+			unusedurl.append(temp)
+	tempcur.close()
+	conn.close()
 
-response=urllib2.urlopen("http://www.baidu.com/")
-html=response.read();
-m=re.findall(r"href='[^']*'",html)
+def get_unusedurl():
+	conn=mysql.connector.connect(g_mysql)
+	tempcur=conn.cursor()
+	tempcur.execute('select * from unusedurl')
+	unusedurl=tempcur.fetchall()
+	tempcur.close()
+	conn.close()
+
+def store_usedurl(url1,url2):
+	conn=mysql.connector.connect(g_mysql)
+	tempcur=conn.cursor()
+	for tt in url1:
+		tempcur.execute("insert into use
+	tempcur.close()
+	conn.close()
+
+def store_unusedurl(url1,url2):
+	conn=mysql.connector.connect(g_mysql)
+	tempcur=conn.cursor()
+	for tt in url1:
+		tempcur.execute("insert into unusedurl('usedurl','shijian','laiyuan') values('"+tt+","+time.time()+","+url2+"')")
+	tempcur.close()
+	conn.close()
+
+print "1 start from a rooturl"
+print "2 start from mysqldb"	
+print "chose the mode"
+type=raw_input()
+if type=="1":
+	print "enter the root url"
+	rooturl=raw_input()	
+	unusedurl.append(rooturl)
+else:
+	get_unusedurl()
+
+while 1:
+	if len(unusedurl)==0:
+		get_unusedurl()
+	tempurl=unusedurl.pop()
+	print "get url "+tempurl
+	response=urllib2.urlopen(tempurl)
+	print "get html"
+	html=response.read();
+	print "analyse html"
+	m=re.findall(r"href='[^']*'",html)
+	check_exist(m)
+	print "check urls"
+	print "current url :"+tempurl		
 
