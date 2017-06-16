82b781d5ddc2cbeb08790979f1aad2b0eeb6c23d
lixiang23 <lixiang23@staff.sina.com.cn>
2017-06-16 13:15:24 +0800

    更新

--- bash/mm.sh
+++ bash/mm.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 #set -x
 #set -e
+
 #监控本地服务器相关指标，生成数据并转储到ElasticSearch中以便实时监控
 
-#配置本机名称，如果配置为空则使用默认主机名'uname -n'
-machine_name=''
-machine_ip='10.13.3.25'
-
-#stat_item 统计指标,统计内容包括:cpu,mem,disk,net各项通过逗号隔开，比如:
-stat_item=cpu,mem,disk,net
-
-#ElasticSearch相关配置，包括主机ip，基础索引等信息
-es_addr='http://es.sina.com.cn'
-#假定es_index='example',则存入ES后的索引为example-20170426
-es_index='mc'
-
+#导入配置文件
+source mm.conf
 
 #程序运行局部变量，不要做任何修改
 cpu_info=''
@@ -22,18 +13,20 @@ mem_info=''
 disk_info=''
 net_info=''
 
+#统计CPU相关
 function statCpu(){
     load_avg_info=`cat /proc/loadavg`
     load_avg_array=(${load_avg_info})
-    cpu_info='"loadavg_1":"'${load_avg_array[0]}'","loadavg_5":"'${load_avg_array[1]}'","loadavg_15":"'${load_avg_array[2]}'"'
+    cpu_info='"loadavg_1":'${load_avg_array[0]}',"loadavg_5":'${load_avg_array[1]}',"loadavg_15":'${load_avg_array[2]}
 }
 
+#统计内存相关
 function statMem(){
     while read line
     do
         mem_item=`echo $line | awk '{print $1}'`
         mem_value=`echo $line | awk '{print $2}'`
-        temp_mem_info='"'$mem_item'":"'$mem_value'"'
+        temp_mem_info='"'$mem_item'":'$mem_value
         if [[ $mem_info = '' ]];then
             mem_info=$temp_mem_info
         else
@@ -42,12 +35,13 @@ function statMem(){
     done < "/proc/meminfo"
 }
 
+#统计硬盘相关
 function statDisk(){
     disk_source_info=`df | sed -n '2,$p' | awk '{print $6","$2","$4","$5}'`
     for item in `echo $disk_source_info | awk '{for(i=0;i<=NF;i++) print $i}'`
     do
         single_disk_info=(${item//,/ })
-        disk_result_info='"'${single_disk_info[0]}'":{"total":"'${single_disk_info[1]}'","ava_size":"'${single_disk_info[2]}'","usage":"'${single_disk_info[3]}'"}'
+        disk_result_info='"'${single_disk_info[0]}'":{"total":'${single_disk_info[1]}',"ava_size":'${single_disk_info[2]}',"usage":'${single_disk_info[3]//%/}'}'
         if [[ $disk_info = '' ]]; then
             disk_info=$disk_result_info
         else
@@ -56,30 +50,37 @@ function statDisk(){
     done
 }
 
+#统计网络相关
 function statNet(){
     net_info='"input":"0","output":"0"'
 }
 
+#存储数据到ES
 function storeES(){
-    current_time=`date +"%Y-%m-%dT%H:%M:%SZ"`
+    #current_time=`date +"%Y-%m-%dT%H:%M:%SZ"`
+    current_time=`date -Ins`
     current_date=`date +%Y-%m-%d`
-    echo $current_time
     json_result='{"@timestamp":"'$current_time'","machine_name":"'$machine_name'","ip":"'$machine_ip'","disk_info":{'$disk_info'},"mem_info":{'$mem_info'},"cpu_info":{'$cpu_info'},"net_info":{'$net_info'}}'
     echo $json_result
 
-    echo `curl -l -H "Content-type: application/json" -X POST -d $json_result $es_addr/$es_index-$current_date/$machine_name`
+    #echo `curl -l -H "Content-type: application/json" -X POST -d $json_result $es_addr/$es_index-$current_date/$machine_name`
 }
 
+#获取机器名字
 function getMachineName(){
-    if [[ $machine_name = '' ]] ; then
+    if [[ $machine_name == '' ]] ; then
         machine_name=`uname -n`
     fi
 }
 
+#获取机器IP地址
 function getMachineIP(){
-    machine_ip='10.13.3.25'
+    if [[ $machine_ip == '' ]];then
+        machine_ip=`ifconfig | grep "inet addr" | awk '{print $2}' | awk -F ':' '{print $2}' | grep -v '127.'| grep -v '172.' | grep -v '192.'`
+    fi
 }
 
+#初始化，获取机器名字及其IP地址
 function init(){
     getMachineName
     getMachineIP
